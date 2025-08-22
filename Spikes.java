8b86a50543324fd7220953a61352283d95f30f48
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:21:28 -0700

    Create Spikes.java

---
+++ Spikes.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A hazard that damages the player when touched.
+ * 
+ * @author Marcus Jones Jr.
+ * @version 1.0
+ */
+public class Spikes extends Actor
+{
+}
