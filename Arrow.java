dbf39f187601b5894cc86c2d6817b8c5d1a94907
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:02:36 -0700

    Create Arrow.java

---
+++ Arrow.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A visual object containing no code. Highlights things in the world.
+ * 
+ * @author Marcus Jones Jr.
+ * @version 1.0
+ */
+public class Arrow extends Actor
+{
+}
