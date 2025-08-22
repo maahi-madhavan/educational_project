25ffe234fd6d7e2b1f9a25b0b1f9ba2bda3da7c2
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:15:08 -0700

    Create Ladder.java

---
+++ Ladder.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A ladder that the player can climb.
+ * 
+ * @author Marcus Jones Jr.
+ * @version 1.0
+ */
+public class Ladder extends Actor
+{
+}
