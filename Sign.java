476a05ce5e535fb826de55454a2beb5c8d6204d0
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:21:13 -0700

    Create Sign.java

---
+++ Sign.java
@@ -0,0 +1,12 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A visual indicator to explain assist players in 
+ * discovering the Switch object.
+ * 
+ * @author Marcus Jones Jr. 
+ * @version 1.0
+ */
+public class Sign extends Actor
+{
+}
