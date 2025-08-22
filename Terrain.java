96108595638184ccbf11bfdc5659125bc718fd56
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:20:14 -0700

    Create Terrain.java

---
+++ Terrain.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A large basic platform.
+ * 
+ * @author Marcus Jones Jr.
+ * @version 1.0
+ */
+public class Terrain extends Platform
+{
+}
