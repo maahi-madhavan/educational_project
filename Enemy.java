6098ea2f1ba7937c3bec43811006404eb50a2735
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:16:11 -0700

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The enemy class is used to classifiy objects as enemies, allowing all of them to damage the player.
+ * 
+ * @author Marcus Jones Jr.
+ * @version 1.0
+ */
+public class Enemy extends Mover
+{
+}
