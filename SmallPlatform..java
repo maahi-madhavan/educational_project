b40f468991420b368d02cd9601c5e38821860912
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:19:07 -0700

    Create SmallPlatform..java

---
+++ SmallPlatform..java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A small basic platform
+ * 
+ * @author Marcus Jones Jr. 
+ * @version 1.0
+ */
+public class SmallPlatform extends Platform
+{
+}
