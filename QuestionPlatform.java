150dbcbaca06a80a77a150f978219d351aae5279
meghna-magesh <148259033+meghna-magesh@users.noreply.github.com>
2025-08-22 11:01:38 -0700

    Update QuestionPlatform.java

--- QuestionPlatform.java
+++ QuestionPlatform.java
@@ -6,19 +6,11 @@ public class QuestionPlatform extends SmallPlatform {
     public void act() {
         checkCollision();
     }
-
     private void checkCollision() {
         if (!triggered) {
             Player player = (Player)getOneIntersectingObject(Player.class);
             if (player != null) {
                 triggered = true;
-
-                // Change platform image to green
-                GreenfootImage img = getImage();
-                img.setColor(Color.GREEN);
-                img.fill(); // fills the image with green
-                setImage(img);
-
                 // Switch to CompSciQ world
                 Greenfoot.setWorld(new CompSciQ());
             }
