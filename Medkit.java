c590d1ed0bc17e0263994e286af2c8ebc825f072
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:15:26 -0700

    Create Medkit.java

---
+++ Medkit.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Restores player Hp when touched.
+ * 
+ * @author Marcus Jones Jr. 
+ * @version 1.0
+ */
+public class Medkit extends Actor
+{
+}
