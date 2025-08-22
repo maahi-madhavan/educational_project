7753d439c313a8aaa5dabd595e6e14167a424e68
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:18:45 -0700

    Create Platform.java

---
+++ Platform.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A basic platform that acts as the parent class to every other platform.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Platform extends Actor
+{
+}
