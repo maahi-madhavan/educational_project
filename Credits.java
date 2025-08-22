d55b4f1dc646856f3b98870bf922cbbd6ac02d13
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:22:45 -0700

    Create Credits.java

---
+++ Credits.java
@@ -0,0 +1,28 @@
+/**
+ * This class contains a comment that provides links to every asset used 
+ * to create this game that was not created by me.
+ * 
+ * @author Marcus Jones Jr. 
+ * @version 1.0
+ */
+public class Credits  
+{
+    /**
+     * Credits
+     * https://opengameart.org/content/hero-character-sprite-sheet
+     * https://opengameart.org/content/beating-heart-0
+     * https://opengameart.org/content/low-poly-medkit-3
+     * https://opengameart.org/content/icy-demon-animated-classic-hero-edit
+     * https://opengameart.org/content/rope-ladder
+     * https://opengameart.org/content/bullet-collection-2-m484-games
+     * https://opengameart.org/content/3-rpg-enemy-remixes
+     * https://opengameart.org/content/colt-1851-navy
+     * https://opengameart.org/content/microgame-pixelgraphicsda
+     * https://www.google.com/url?sa=i&url=https%3A%2F%2Fthenerdstash.com%2Fhow-to-get-use-society-medallions%2F&psig=AOvVaw2snO3oq-VLJyzblfo4KLJb&ust=1703079999544000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCLjH28fRm4MDFQAAAAAdAAAAABAD
+     * https://tuna.voicemod.net/sound/66a16121-f874-4b47-b405-571c02a2adf1
+     * https://opengameart.org/content/mountain-at-dusk-background
+     * 
+     * Sound effect program: https://www.bfxr.net/
+     * Sprite editors: https://libresprite.github.io/#!/ https://www.piskelapp.com/
+     */
+}
