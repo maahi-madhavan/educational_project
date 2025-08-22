1b869b6b198b06ba660492d0517f1404f7ec9bd1
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:14:37 -0700

    Create Goal.java

---
+++ Goal.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The main objective for the player. 
+ * 
+ * @author Marcus Jones Jr.
+ * @version 1.0
+ */
+public class Goal extends Actor
+{
+}
