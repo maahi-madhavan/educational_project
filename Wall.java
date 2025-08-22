32c5c39815908d3ea2955f54e0f0fc76f2e9179e
maahi-madhavan <148258518+maahi-madhavan@users.noreply.github.com>
2025-08-21 21:20:31 -0700

    Create Wall.java

---
+++ Wall.java
@@ -0,0 +1,17 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * A vertical wall that the player cannot pass.
+ * 
+ * @author (Marcus Jones Jr. 
+ * @version 1.0
+ */
+public class Wall extends Platform
+{
+    /**
+     * This is the constructor for the Wall class.
+     */
+    public Wall(){
+        setRotation(90);
+    }
+}
